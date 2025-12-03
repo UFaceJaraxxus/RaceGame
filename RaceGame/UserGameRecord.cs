37aa1d562e6567f7df38502e8278cade52e9bf7e
Андрей Ананьев <Andrew.Pascal94@gmail.com>
2025-12-04 00:44:38 +0300

    Выполнил задания 2,3,4

---
+++ RaceGame/UserGameRecord.cs
@@ -0,0 +1,18 @@
+﻿namespace Race
+{
+    public class UserGameRecord
+    {
+        public User Player { get; set; }
+        public int Score { get; set; }
+        public int Coins { get; set; }
+        public DateTime RecordDate { get; set; }
+
+        public UserGameRecord(User player, int score, int coins, DateTime recordDate)
+        {
+            Player = player;
+            Score = score;
+            Coins = coins;
+            RecordDate = recordDate;
+        }
+    }
+}
